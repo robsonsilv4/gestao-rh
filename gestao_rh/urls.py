ba2bd01c16d8cb7592e9af9000c4213faa967ee1
robsonsilv4 <robsonsilv410@gmail.com>
2019-08-23 16:35:33 -0300

    Adicionado páginas de login e logout

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.base.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
+    path('contas/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
