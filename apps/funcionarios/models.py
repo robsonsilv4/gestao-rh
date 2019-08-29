3f7c529b37451ec0b9b6eb9dddac5e644716203c
robsonsilv4 <robsonsilv410@gmail.com>
2019-08-29 16:17:26 -0300

    Adicionado total de horas na edição

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Sum
 from django.urls import reverse
 
 from apps.departamentos.models import Departamento
@@ -11,6 +12,10 @@ class Funcionario(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    @property
+    def total_horas(self):
+        return self.horaextra_set.all().aggregate(Sum('horas'))['horas__sum']
 
     def __str__(self):
         return self.nome
