02ee7051d8aec25ae4a8d35073e309135803279f
robsonsilv4 <robsonsilv410@gmail.com>
2019-08-29 16:54:37 -0300

    Adicionado redirecionamentos para horas

--- apps/horas_extras/urls.py
+++ apps/horas_extras/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (HoraExtraListView,
                     HoraExtraCreateView,
                     HoraExtraUpdateView,
+                    HoraExtraPaginaUpdateView,
                     HoraExtraDeleteView)
 
 app_name = 'horas_extras'
@@ -10,5 +11,6 @@ urlpatterns = [
     path('', HoraExtraListView.as_view(), name='listar'),
     path('adicionar/', HoraExtraCreateView.as_view(), name='adicionar'),
     path('atualizar/<int:pk>', HoraExtraUpdateView.as_view(), name='atualizar'),
+    path('atualizar-pagina/<int:pk>', HoraExtraPaginaUpdateView.as_view(), name='atualizar-pagina'),
     path('remover/<int:pk>', HoraExtraDeleteView.as_view(), name='remover'),
 ]
--- apps/horas_extras/views.py
+++ apps/horas_extras/views.py
@@ -28,9 +28,23 @@ class HoraExtraUpdateView(UpdateView):
     form_class = HoraExtraForm
 
     def get_form_kwargs(self):
-        kwargs = super(HoraExtraCreateView, self).get_form_kwargs()
+        kwargs = super(HoraExtraUpdateView, self).get_form_kwargs()
         kwargs.update({'user': self.request.user})
         return kwargs
+
+
+class HoraExtraPaginaUpdateView(UpdateView):
+    model = HoraExtra
+    form_class = HoraExtraForm
+    # success_url = reverse_lazy('horas_extras:atualizar')
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraPaginaUpdateView, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def get_success_url(self):
+        return reverse_lazy('horas_extras:atualizar-pagina', args=[self.object.id])
 
 
 class HoraExtraDeleteView(DeleteView):
